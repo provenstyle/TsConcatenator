902a1447a325c981b9173ae52f47f11cddbdd646
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-08-05 14:07:38 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TsFileConcatenator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Environment.CurrentDirectory;
+            Console.WriteLine("Current Directory: {0}", currentDirectory);
+            var files = Directory.GetFiles(currentDirectory, "*.ts");
+            files.ToList().ForEach(f=> Console.WriteLine("ts file: {0}", f));
+
+            if (files.Any())
+            {
+                var outputFile = Path.Combine(currentDirectory, "combined.ts");
+                Console.WriteLine("Resultant file: {0}", outputFile);
+
+                ConcatenateFiles(outputFile, files);
+            }
+            else
+            {
+                Console.WriteLine("No ts files to concatenate in: {0}", currentDirectory);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static void ConcatenateFiles(string outputFile, IEnumerable<string> inputFiles)
+        {
+            using (Stream output = File.OpenWrite(outputFile))
+            {
+                foreach (var inputFile in inputFiles)
+                {
+                    if (!File.Exists(inputFile))
+                    {
+                        Console.WriteLine(string.Format("File Not Found: {0}", inputFile));
+                        continue;
+                    }
+                    using (var input = File.OpenRead(inputFile))
+                        input.CopyTo(output);
+                }
+            }
+        }
+    }
+}
